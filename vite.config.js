4ac64c6bb0e7794c7755820c4f34e0dde89c24d3
Nadia Soledad Pereyra <nadiapereyra32@gmail.com>
2025-11-08 15:32:49 -0300

    actualizo vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,7 @@
-
 import { defineConfig } from 'vite'
-
 import react from '@vitejs/plugin-react'
 
-
-
-// https://vitejs.dev/config/
-
+// https://vite.dev/config/
 export default defineConfig({
-
   plugins: [react()],
-
-  server: {
-
-    proxy: {
-
-      '/api': {
-
-        target: 'https://api-crud-gestion-escolar.vercel.app',
-
-        changeOrigin: true,
-
-        secure: true,
-
-        rewrite: (path) => path.replace(/^\/api/, '/api')
-
-      }
-
-    }
-
-  }
-
 })
